264ee79841a9dff18aecb0596ce5f7712c0792d9
TheAnachronisticEngineer <aw815@york.ac.uk>
2014-02-24 12:39:04 +0000

    added JButtons to UtilitiesPanel. Created GUI.Java

--- src/src/UtilitiesPanel.java
+++ src/src/UtilitiesPanel.java
@@ -3,11 +3,15 @@ package src;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
  * 
  * @author Sam Pick
+ * @author Andrew Walter
  *
  */
 public class UtilitiesPanel extends JPanel implements ActionListener{
@@ -25,14 +29,32 @@ public class UtilitiesPanel extends JPanel implements ActionListener{
 	 * Create a simple panel and add the JButtons for the utilities
 	 */
 	public UtilitiesPanel() {
+		//Calls JPanel to create the UtilitiesPanel object.
+		//Sets the layout to BoxLayout
 		super();
+		UtilitiesPanel.setLayout(new BoxLayout(UtilitiesPanel, BoxLayout.Y_AXIS));
 		
 		// When the panel is first created the utilities will not exist but when they are
 		// instantiated, these fields should be updated so we don't get multiple utilities
 		diceRollerExists = false;
 		calculatorExists = false;
 		
-		// TODO add the necessary JButtons
+		//Adds JButtons for diceRoller and calculator
+		JButton diceButton = new JButton("Launch Dice Roller");
+		diceButton.setVerticalTextPosition(AbstractButton.CENTER);
+		diceButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		diceButton.setActionCommand("diceRollerLaunch");
+		diceButton.addActionListener(this);
+		diceButton.setToolTipText("Opens the dice roller application");
+		UtilitiesPanel.add(contrastButton);
+		
+		JButton calculatorButton = new JButton("Launch Modifier Calculator");
+		calculatorButton.setVerticalTextPosition(AbstractButton.CENTER);
+		calculatorButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		calculatorButton.setActionCommand("calculatorLaunch");
+		calculatorButton.addActionListener(this);
+		calculatorButton.setToolTipText("Opens the modifier calculator application");
+		UtilitiesPanel.add(calculatorButton);
 	}
 	
 	
@@ -47,9 +69,9 @@ public class UtilitiesPanel extends JPanel implements ActionListener{
 	}
 
 
-	public void setVisibility(boolean visible){
-			this.setVisible(visible);
-		}
+	//public void setVisibility(boolean visible){
+	//		this.setVisible(visible);
+	//	}
 
 	
 	
