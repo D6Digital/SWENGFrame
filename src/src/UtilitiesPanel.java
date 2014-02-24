385546f48b166b2846f82088c5abad85697b9d79
TheAnachronisticEngineer <aw815@york.ac.uk>
2014-02-24 15:07:29 +0000

    Fixed errors in GUI & UtilitiesPanel Added Main() to Overall

--- src/src/UtilitiesPanel.java
+++ src/src/UtilitiesPanel.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.AbstractButton;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
