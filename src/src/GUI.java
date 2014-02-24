f847045e5d36006eeffdc46e95de2b78579f6b1a
TheAnachronisticEngineer <aw815@york.ac.uk>
2014-02-24 13:22:43 +0000

    Wrote GUI.java

---
+++ src/src/GUI.java
@@ -0,0 +1,67 @@
+package src;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+/**
+ * 
+ * @author Andrew Walter
+ *
+ */
+public class GUI extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Container contentPane;
+	string panelType = "utilitiesSelectionPanel";
+	string windowName = "Utilities";
+	int width = 200;
+	int height = 250;
+	
+	/**
+	 * Create a simple JFrame and then populate it with specified JPanel type
+	 * string panelType, string windowName int width, int height
+	 */
+	public GUI() {
+		setTitle("Grimoire - " + windowName);
+		setSize(width, height);
+		setVisible(true);
+		
+		contentPane = getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		
+		switch (panelType) {
+			case bookSelectionPanel:    
+				//BookPanel();
+				break;
+			case mainMenuPanel:          
+				//MenuPanel();
+				break;
+			case bookMainPanel:          
+				//ContentsPanel();
+				//ControlPanel();
+				break;
+			case videoDisplayPanel:      
+				//VideoPanel();
+				break;
+			case audioMenuPanel:         
+				//AudioPanel();
+				break;
+			case utilitiesSelectionPanel:
+				UtilitiesPanel utilitiesSelectionPanel = new UtilitiesPanel();
+				contentPane.add(utilitiesSelectionPanel, BorderLayout.EAST);
+				break;
+			case diceRollerPanel:        
+				//DicePanel();
+				break;
+			case calculatorPanel:        
+				//CalculatorPanel();
+				break;
+			default:                     
+				//???DefaultPanel()???
+				break;
+		};
+	}
+}
