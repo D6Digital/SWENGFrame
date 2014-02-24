f3265df793e4063124018b7bc3d388c3130177c7
jfd501 <jfd501@york.ac.uk>
2014-02-24 15:26:15 +0000

    Added fields and methods for height and width

--- src/src/Image.java
+++ src/src/Image.java
@@ -4,19 +4,69 @@
 package src;
 
 /**
- * @author Robert
+ * @author Robert, Josh Drake
  *
  */
 public class Image extends SlideObject {
+	private int x_coord;
+	private int y_coord;
+	private String file;
+	private int width;
+	private int height;
 
 	/**
+	 * 
 	 * @param x_coord
 	 * @param y_coord
 	 * @param file
+	 * @param width
+	 * @param height
 	 */
-	public Image(int x_coord, int y_coord, String file) {
+	public Image(int x_coord, int y_coord, String file, int width, int height) {
 		super(x_coord, y_coord, file);
-		// TODO Auto-generated constructor stub
+		this.x_coord = x_coord;
+		this.y_coord = y_coord;
+		this.file = file;
+		this.width = width;
+		this.height = height;
+		
+	}
+	/**
+	 * @return the x_coord
+	 */
+	public int getX_coord() {
+		return x_coord;
 	}
 
+	/**
+	 * @return the y_coord
+	 */
+	public int getY_coord() {
+		return y_coord;
+	}
+	
+	/**
+	 * @return the file
+	 */
+	
+	public String getFile() {
+		return file;
+	}	
+
+	/**
+	 * 
+	 * @return the width
+	 */
+	public int getWidth(){
+		return width;
+	}
+	
+	/**
+	 * 
+	 * @return the height
+	 */
+	public int getHeight(){
+		return height;
+	}
+	
 }
