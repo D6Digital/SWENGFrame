5e5d67ff4fb9d091dec1c232c9c2b68ed6abdc1f
propellerhead <rjm529@york.ac.uk>
2014-02-26 11:03:56 +0000

    Formalised javadoc for slide object

--- src/slideModule/SlideObject.java
+++ src/slideModule/SlideObject.java
@@ -4,7 +4,7 @@
 package slideModule;
 
 /**
- * @author Robert
+ * @author Robert Mills
  *
  */
 public class SlideObject {
@@ -26,6 +26,40 @@ public class SlideObject {
 		this.file = file;
 	}
 	/**
+	 * Sets the x coordinate of the slide object.
+	 * <p>
+	 * For music files this will be ignored
+	 * @param x_coord the x-coordinate
+	 */
+	public void setX_coord(int x_coord) {
+		this.x_coord = x_coord;
+	}
+	/**
+	 * Sets the y coordinate of the slide object
+	 * <p>
+	 * For music files this will be ignored
+	 * @param y_coord the y-coordinate
+	 */
+	public void setY_coord(int y_coord) {
+		this.y_coord = y_coord;
+	}
+	/**
+	 * Sets a file name for an object.
+	 * The file it represents will be as follows
+	 * <ul>
+	 * <li> Text: the font file
+	 * <li> Image: the image file
+	 * <li> Audio: the sound file
+	 * <li> Video: the video file
+	 * <li> Graphics: do not care
+	 * </ul>
+	 * @param file the file name including any change from current file directory
+	 */
+	public void setFile(String file) {
+		this.file = file;
+	}
+	/**
+	 * Retrieves the x coordinate of the slide object
 	 * @return the x_coord
 	 */
 	public int getX_coord() {
@@ -33,12 +67,14 @@ public class SlideObject {
 	}
 
 	/**
+	 * Retrieves the y-coordinate of the slide object
 	 * @return the y_coord
 	 */
 	public int getY_coord() {
 		return y_coord;
 	}
 	/**
+	 * Retrieves the associated filename ofr the slide object
 	 * @return the file
 	 */
 	public String getFile() {
