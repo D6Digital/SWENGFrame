222074d78e7b92ac84ee8ff938422344c9b6c693
samPick <sp807@.york.ac.uk>
2014-03-06 18:22:05 +0000

    Added a font test in Scribe

--- src/textModule/Scribe.java
+++ src/textModule/Scribe.java
@@ -1,8 +1,18 @@
 package textModule;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.awt.LayoutManager;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.text.AttributeSet;
@@ -23,9 +33,32 @@ public class Scribe extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+	Font font;
 
 	public Scribe() {
+		
+		
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		try {
+			//font = Font.createFont(Font.TRUETYPE_FONT, new File("M:\\JAVA\\SWENG\\SWENGFrame\\src\\textModule\\ChineseTakeaway.ttf"));
+			//font.deriveFont(Font.PLAIN,24);
+			//InputStream myStream = new BufferedInputStream(new FileInputStream("C:\\xtemp\\NINJAS.TTF"));
+			
+			
+		    font = Font.createFont(Font.TRUETYPE_FONT, new File("space age.ttf"));
+		    font = font.deriveFont(Font.PLAIN,50);
+
+			ge.registerFont(font);
+		} catch (FontFormatException | IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		
+		
+		
+		
+		
 		
 		setLayout(new BorderLayout());
 		
@@ -69,18 +102,24 @@ public class Scribe extends JPanel {
 		Style def = StyleContext.getDefaultStyleContext().getStyle(StyleContext.DEFAULT_STYLE);
 		
 		Style myStyle = doc.addStyle("myStyle", def);
-		StyleConstants.setFontFamily(myStyle,"SansSerif");
-		StyleConstants.setFontSize(myStyle, 26);
+		StyleConstants.setFontFamily(myStyle,"space age");
+		StyleConstants.setFontSize(myStyle, 60);
 		StyleConstants.setBold(myStyle, true);
+		StyleConstants.setForeground(myStyle, Color.CYAN);
 		
 	}
 	
 	public static void showAndDisplayGUI()
 	{
+		System.out.println(System.getProperty("user.dir"));
 		JFrame frame = new JFrame();
 		frame.setSize(320, 240);
+		frame.setLayout(new BorderLayout());
 		Scribe scribeTest = new Scribe();
 		frame.add(scribeTest);
+		JLabel fontLabel = new JLabel(scribeTest.font.getFamily());
+		fontLabel.setFont(scribeTest.font);
+		//frame.add(fontLabel);
 		frame.setVisible(true);
 	}
 	
