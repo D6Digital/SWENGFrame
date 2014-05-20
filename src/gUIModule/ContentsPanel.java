56a61bc443a5c495bcb9a48bb16ce2a4ab019369
jfd501 <jfd501@york.ac.uk>
2014-05-20 16:22:57 +0100

    Content panel changed to include a list instead of buttons, a test created which puts the panel on a frame.

--- src/gUIModule/ContentsPanel.java
+++ src/gUIModule/ContentsPanel.java
@@ -3,13 +3,20 @@ package gUIModule;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.AbstractButton;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+import presentation.Presentation;
 import presentation.Slide;
 
 
@@ -28,6 +35,11 @@ public class ContentsPanel extends JPanel implements ActionListener{
 	private ArrayList<Slide> contentsSlideList;
 	SlidePanel slidePanel1;
 	SlidePanel slidePanel2;
+	JButton mainMenuButton = new JButton("Main Menu");
+	JLabel systemLabel = new JLabel();
+	JLabel bookLabel = new JLabel();
+	DefaultListModel listModel = new DefaultListModel<String>();
+	JList contentsList = new JList(listModel);
 	
 	
 	/**
@@ -35,14 +47,23 @@ public class ContentsPanel extends JPanel implements ActionListener{
 	 * @param slide1
 	 * @param slide2
 	 */
-	public ContentsPanel(SlidePanel slide1, SlidePanel slide2, ArrayList<Slide> contentSlideList) {
+	public ContentsPanel(ArrayList<Slide> contentSlideList) {
 		super();
+		this.setLayout(null);
 		contentsSlideList = contentSlideList;
-		slidePanel1 = slide1;
-		slidePanel2 = slide2;
+		//slidePanel1 = slide1;
+		//slidePanel2 = slide2;
+		mainMenuButton.setBounds(25, 50, 100, 40);
+		systemLabel.setBounds(10,120,130,40);
+		bookLabel.setBounds(10,190,130,40);
+		systemLabel.setText("System:");
+		bookLabel.setText("Book:");
+		this.add(mainMenuButton);
+		this.add(systemLabel);
+		this.add(bookLabel);
 		
 		JScrollPane contents = createScrollPane(contentsSlideList);
-		
+		contents.setBounds(0,260,220,200);
 		this.add(contents);
 		// TODO add a title JLabel and ensure the panel is ready
 	}
@@ -60,36 +81,70 @@ public class ContentsPanel extends JPanel implements ActionListener{
 	 */
 	private JScrollPane createScrollPane(ArrayList<Slide> contentsSlideList) {
 		JScrollPane contents = new JScrollPane();
+		
+		listModel.clear();
+		contentsList.removeAll();
 
+		
 		//Cycle through all slides in the contents list and creates a JButton for each 
-
-		for (int i = 0; i < contentsSlideList.size(); i++) {
-			final Slide currentSlide = contentsSlideList.get(i);
-			final String currentTitle = (String) contentsSlideList.get(i).getSlideName();
-			JButton currentButton = new JButton(currentTitle);
-			currentButton.setVerticalTextPosition(AbstractButton.CENTER);
-			currentButton.setHorizontalTextPosition(AbstractButton.CENTER);
-			currentButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-			currentButton.setToolTipText("Go to " + currentTitle);	
-			
-			//Action listener will display the relevant slide when a button is pressed
-	        currentButton.addActionListener(new ActionListener() {
-	        	 
-	            public void actionPerformed(ActionEvent e)
-	            {
-	                //Execute when button is pressed
-	                System.out.println("You chose " + currentTitle);
-	                SlidePanel newSlide = new SlidePanel();
-	                newSlide.refreshSlide(currentSlide);
-	                
-	            }
-	        });
-	        
-		//Add to contents JScrollPane	
-			contents.add(currentButton);
+		for (Slide currentSlide : contentsSlideList) {
+			listModel.addElement(currentSlide.getSlideID() + ". " + currentSlide.getDescriptor());
 		}
+		contents.setViewportView(contentsList);
+		
+		contentsList.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {	}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount() == 2) {
+					int x = contentsList.getSelectedIndex();
+					contentsList.clearSelection();
+					System.out.println("NEW SELECTION MADE: " + x);
+				}
+			}
+		});
 		
 		return contents;
+
+//		for (int i = 0; i < contentsSlideList.size(); i++) {
+//			final Slide currentSlide = contentsSlideList.get(i);
+//			final String currentTitle = (String) contentsSlideList.get(i).getSlideName();
+//			JButton currentButton = new JButton(currentTitle);
+//			currentButton.setVerticalTextPosition(AbstractButton.CENTER);
+//			currentButton.setHorizontalTextPosition(AbstractButton.CENTER);
+//			currentButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+//			currentButton.setToolTipText("Go to " + currentTitle);	
+//			
+//			//Action listener will display the relevant slide when a button is pressed
+//	        currentButton.addActionListener(new ActionListener() {
+//	        	 
+//	            public void actionPerformed(ActionEvent e)
+//	            {
+//	                //Execute when button is pressed
+//	                System.out.println("You chose " + currentTitle);
+//	                SlidePanel newSlide = new SlidePanel();
+//	                newSlide.refreshSlide(currentSlide);
+//	                
+//	            }
+//	        });
+	        
+		//Add to contents JScrollPane	
+//			contents.add(currentButton);
+//		}
+//		
+//		return contents;
 	}
 	
 	/**
---
+++ src/gUIModule/ContentsPanelTest.java
@@ -0,0 +1,50 @@
+package gUIModule;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import presentation.Slide;
+
+public class ContentsPanelTest {
+	
+	ArrayList<Slide> slideList = new ArrayList<>();
+	int slideListSize = 20;
+	JFrame frame = new JFrame();
+	
+	@Before
+	public void before() {
+		
+		for(int i = 0; i < slideListSize; i++) {
+			Slide slide = new Slide();
+			slide.setSlideID(i);
+			slide.setDescriptor("DESCRIPTION: " + i);
+			slideList.add(slide);
+		}
+		
+		
+	}
+	
+	@Test
+	public void test() {
+		
+		ContentsPanel contentsPanel = new ContentsPanel(slideList);
+		
+		frame.setSize(400, 400);
+		frame.add(contentsPanel);
+		frame.setVisible(true);
+		
+		boolean x = true;
+		do{
+			
+		}
+		while(x);
+		
+	}
+
+}
