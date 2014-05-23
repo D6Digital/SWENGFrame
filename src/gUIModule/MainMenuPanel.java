0fba776a2010bd42ecfd50e17b02489c4dc5befd
jfd501 <jfd501@york.ac.uk>
2014-05-23 16:24:55 +0100

    Changes to the Main menu GUI

--- src/gUIModule/MainMenuPanel.java
+++ src/gUIModule/MainMenuPanel.java
@@ -4,13 +4,20 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import presentation.Slide;
 
 public class MainMenuPanel extends JPanel{
 	
@@ -18,6 +25,15 @@ public class MainMenuPanel extends JPanel{
 	JLabel background;
 	JLabel logoLabel;
 	JLayeredPane layers = new JLayeredPane();
+	JTextField description = new JTextField();
+	DefaultListModel systemListModel = new DefaultListModel<String>();
+	JList systemList = new JList(systemListModel);
+	JScrollPane systemScroll = new JScrollPane();
+	DefaultListModel bookListModel = new DefaultListModel<String>();
+	JList bookList = new JList(bookListModel);
+	JScrollPane bookScroll = new JScrollPane();
+
+	
 	public MainMenuPanel(int width, int height) {
 		
 		setLayout(null);
@@ -25,9 +41,10 @@ public class MainMenuPanel extends JPanel{
 		layers.setBounds(0, 0, width, height);
 		System.out.println("height="+height + "width="+width);
 		
-		button.setBounds(20, 20, 100, 40);
+		//start button
+		button.setBounds(width-100, height-40, 100, 40);
 		
-		
+		//background
 		BufferedImage backgroundImage;
 		try{
 			backgroundImage = ImageIO.read(new File("resources/buttons/Background.png"));
@@ -38,6 +55,7 @@ public class MainMenuPanel extends JPanel{
 			e2.printStackTrace();
 		}
 		
+		//logo
 		BufferedImage logoImage;
 		try{
 			logoImage = ImageIO.read(new File("resources/buttons/sideLogo.png"));
@@ -48,10 +66,68 @@ public class MainMenuPanel extends JPanel{
 			e2.printStackTrace();
 		}
 		
-		layers.add(logoLabel,2);
-		layers.add(background,1);
-		layers.add(button,0);
-		add(layers);
+		//Descriptions
+		description.setBounds(160, 0, width-250, 100);
+		description.setText("This is a description!!!!!!!!");
+		description.setEditable(false);
+		
+		//Labels
+		JLabel systemLabel = new JLabel("Choose A System:");
+		systemLabel.setBounds(160, 140, 120, 50);
+		JLabel bookLabel = new JLabel("Choose A Book:");
+		bookLabel.setBounds(300, 140, 120, 50);
+		
+		//scrollpanes
+		setUpScrollPanes();
+		
+		//adding	
+		add(bookScroll);
+		add(systemScroll);
+		add(bookLabel);
+		add(systemLabel);
+		add(button);
+		add(description);
+		add(logoLabel);
+		add(background);
+		//add(layers);
+	}
+
+	private void setUpScrollPanes() {
+		//scrollpanes
+
+				systemListModel.clear();
+				systemList.removeAll();
+
+				
+//				//Cycle through all slides in the contents list and creates a JButton for each 
+//				for (Slide currentSlide : contentsSlideList) {
+//					listModel.addElement(currentSlide.getSlideID() + ". " + currentSlide.getDescriptor());
+//				}
+				systemListModel.add(0, new String("hi there"));
+				systemListModel.add(1, new String("bye there"));
+				systemListModel.add(2, new String("hello there"));
+				systemListModel.add(3, new String("goodbye there"));
+				
+				systemScroll.setViewportView(systemList);
+				systemScroll.setBounds(160, 200, 100, 300);
+				
+				bookListModel.clear();
+				bookList.removeAll();
+
+				
+//				//Cycle through all slides in the contents list and creates a JButton for each 
+//				for (Slide currentSlide : contentsSlideList) {
+//					listModel.addElement(currentSlide.getSlideID() + ". " + currentSlide.getDescriptor());
+//				}
+				bookListModel.add(0, new String("hi there"));
+				bookListModel.add(1, new String("bye there"));
+				bookListModel.add(2, new String("hello there"));
+				bookListModel.add(3, new String("goodbye there"));
+				
+				bookScroll.setViewportView(bookList);
+				bookScroll.setBounds(300, 200, 100, 300);
+				
+		
 	}
 
 	public JButton getButton(){
