91a3ef51b0ee6c7d47738031d6b461d4c219017c
samLambertD6D <swjl500@york.ac.uk>
2014-06-10 11:53:45 +0100

    Added comments to textModule

--- src/textModule/Text.java
+++ src/textModule/Text.java
@@ -23,126 +23,199 @@ public class Text {
 	int Layer = -1;
 	ArrayList<TextBody> textbody = new ArrayList<TextBody>();
 	String LineColor = "BLACK";
-	
+
+	/**
+	 * @return the font
+	 */
 	public String getFont()
 	{
 		return Font;
 	}
-	
+
+	/**
+	 * @param set the font
+	 */
 	public void setFont(String font)
 	{
 		Font = font;
 	}
-	
+
+	/**
+	 * @return the font size
+	 */
 	public int getFontSize()
 	{
 		return FontSize;
 	}
-	
+
+	/**
+	 * @param set the font size
+	 */
 	public void setFontSize(int fontsize)
 	{
 		FontSize = fontsize;
 	}
-	
+
+	/**
+	 * @return the font colour
+	 */
 	public String getFontColor()
 	{
 		return FontColor;
 	}
-	
+
+	/**
+	 * @param set the font colour
+	 */
 	public void setFontColor(String fontcolor)
 	{
 		FontColor = fontcolor;
 	}
-	
+
+	/**
+	 * @return the line colour
+	 */
 	public String getLineColor()
 	{
 		return LineColor;
 	}
-	
+
+	/**
+	 * @param set the line colour
+	 */
 	public void setLineColor(String linecolor)
 	{
 		LineColor = linecolor;
 	}
-	
+
+	/**
+	 * @return the X-coordinate of the start position
+	 */
 	public int getXstart()
 	{
 		return Xstart;
 	}
-	
+
+	/**
+	 * @param set the X-coordinate of the start position
+	 */
 	public void setXstart(int xstart)
 	{
 		Xstart = xstart;
 	}
-	
+
+	/**
+	 * @return the Y-coordinate of the start position
+	 */
 	public int getYstart()
 	{
 		return Ystart;
 	}
-	
+
+	/**
+	 * @param set the Y-coordinate of the start position
+	 */
 	public void setYstart(int ystart)
 	{
 		Ystart = ystart;
 	}
+
+	/**
+	 * @return the X-coordinate of the end position
+	 */
 	public int getXend()
 	{
 		return Xend;
 	}
-	
+
+	/**
+	 * @param set the X-coordinate of the end position
+	 */
 	public void setXend(int xend)
 	{
 		Xend = xend;
 	}
-	
+
+	/**
+	 * @return the Y-coordinate of the end position
+	 */
 	public int getYend()
 	{
 		return Yend;
 	}
-	
+
+	/**
+	 * @param set the Y-coordinate of the end position
+	 */
 	public void setYend(int yend)
 	{
 		Yend = yend;
 	}
-	
+
+	/**
+	 * @return the start time of the text object
+	 */
 	public int getStartTime()
 	{
 		return Starttime;
 	}
-	
+
+	/**
+	 * @param set the start time of the text object
+	 */
 	public void setStartTime(int startime)
 	{
 		Starttime = startime;
 	}
-	
+
+	/**
+	 * @return the duration
+	 */
 	public int getDuration()
 	{
 		return Duration;
 	}
-	
+
+	/**
+	 * @param set the duration
+	 */
 	public void setDuration(int duration)
 	{
 		Duration = duration;
 	}
-	
+
+	/**
+	 * @return the layer of the text object
+	 */
 	public int getLayer()
 	{
 		return Layer;
 	}
-	
+
+	/**
+	 * @param set the layer of the text object
+	 */
 	public void setLayer(int layer)
 	{
 		Layer = layer;
 	}
 
+	/**
+	 * @return the body
+	 */
 	public TextBody getBody(int i) 
 	{
 		return textbody.get(i);
 	}
-	
+
+	/**
+	 * @param set the body
+	 */
 	public void setBody(TextBody body)
 	{
 		textbody.add(body);
 	}
-	
+
 	/**
 	 * This is an additional method required by the text module to process the text body
 	 * @return the array list of text body
--- src/textModule/TextBody.java
+++ src/textModule/TextBody.java
@@ -9,58 +9,88 @@ public class TextBody
 	private boolean underlined = false;
 	private int branch = -1;
 
+	/**
+	 * @return the text
+	 */
 	public String getText() 
 	{
 		return text;
 	}
-	
+
+	/**
+	 * @param set the text
+	 */
 	public void setText(String s)
 	{
 		text = s;
 	}
-	
+
+	/**
+	 * @return bold
+	 */
 	public boolean getBold()
 	{
 		return bold;
 	}
 
+	/**
+	 * @param set bold
+	 */
 	public void setBold(boolean b) 
 	{
 		bold = b;
-		
+
 	}
-	
+
+	/**
+	 * @return italic
+	 */
 	public boolean getItalic()
 	{
 		return italic;
 	}
 
+	/**
+	 * @param set italic
+	 */
 	public void setItalic(boolean i) 
 	{
 		italic = i;
-		
+
 	}
-	
+
+	/**
+	 * @return underlined
+	 */
 	public boolean getUnderlined()
 	{
 		return underlined;
 	}
 
+	/**
+	 * @param set underlined
+	 */
 	public void setUnderlined(boolean u) 
 	{
 		underlined = u;
-		
+
 	}
-	
+
+	/**
+	 * @return the branch
+	 */
 	public int getBranch()
 	{
 		return branch;
 	}
 
+	/**
+	 * @param set the branch
+	 */
 	public void setBranch(int branch) 
 	{
 		this.branch = branch;
-		
+
 	}
 
 }
